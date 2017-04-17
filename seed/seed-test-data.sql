e10b8c98eb4ff03e7c536314e63c0a39eae07b13
Zachary Kruise <zachkruise@gmail.com>
2017-04-17 16:09:01 -0400

    Seeded more professionally and InitDB script modified slightly.

--- seed/seed-test-data.sql
+++ seed/seed-test-data.sql
@@ -34,43 +34,67 @@ Insert into Service
 	Values
 	('DTHHMOON','Double Tree Moon Twp.', 'Double Tree Moon Twp.','8402 University Blvd, Moon, PA 15108',TRUE),
 	('LQInnPittAir','La Quinta Inn Pittsburgh Airport','La Quinta Inn Pittsburgh Airport','8507 University Blvd, Moon, PA 15108', True),
-	('SheratonPittAir', 'Sheraton Pittsburgh Airport','Sheraton Pittsburgh Airport','1160 Thorn Run Rd, Coraopolis, PA 15108',True),
-	('RenPitt','Renaissance Pittsburgh Hotel','Renaissance Pittsburgh Hotel','107 6th St, Pittsburgh, PA 15222', False),
-	('clarionshuttle','Clarion Shuttle Company','clarionshuttle', '840 Wood Street, Clarion, PA 16214', true),
-	('old_clarionshuttle','Old Clarion Shuttles','old_clarionshuttles','840 Wood Street, Clarion, PA 16214', false);
+	('clarionshuttle','Clarion Shuttle Company','clarionshuttle', '840 Wood Street, Clarion, PA 16214', true);
 
 Insert into public.User
 	( ServiceID , FName , LName , UserName , "Password" , UserType )
 	Values
-	(1,'Zachary','Kruise','zkruise','bigboy','DRIVER'),
-	(1,'Bachary','Bruise','bkruise','bigboy1','DRIVER'),
-	(2,'Cachary','Cruise','ckruise','bigboy2','DISPATCHER'),
-	(2,'Dachary','Druise','dkruise','bigboy2','DRIVER'),
-	(1,'Eachary','Eruise','ekruise','bigboy3','DISPATCHER'),
-	(1,'Fachary','Fruise','fkruise','bigboy4','DRIVER'),
-	(2,'Gachary','Gruise','gkruise','bigboy2','DRIVER'),
-	(2,'Hachary','Hruise','hkruise','bigboy5','DRIVER'),
-	(5,'Travis','Caro','tcaro','password1','DISPATCHER'),
-	(5,'Travis','Caro','tcaro_driver','password1','DRIVER');
+	 (1, 'Jennifer', 'Olson', 'jolsonb', 'OsrHTfhkiFWN', 'DISPATCHER'),
+	 (1, 'Katherine', 'Lopez', 'klopezg', 'JXVLvlBkMoC', 'DISPATCHER'),
+	 (1, 'Kevin', 'Palmer', 'kpalmerk', 'bk03RkXWefc', 'DISPATCHER'),
+	 (1, 'Amy', 'Barnes', 'abarnes0', '2LseBl0vPJ5o', 'DRIVER'),
+	 (1, 'Nancy', 'Alvarez', 'nalvareza', 'MTyDwGmudyWc', 'DRIVER'),
+	 (2, 'Matthew', 'Gutierrez', 'mgutierrez5', '5vVIJCsearh', 'DISPATCHER'),
+	 (2, 'Evelyn', 'Armstrong', 'earmstronge', 'hexi8bCZP6wC', 'DISPATCHER'),
+	 (2, 'Pamela', 'Johnson', 'pjohnsonm', 'hlQY4sdXcbZJ', 'DISPATCHER'),
+	 (2, 'Wanda', 'Griffin', 'wgriffino', '8DhEhQ', 'DISPATCHER'),
+	 (2, 'Dennis', 'Anderson', 'danderson2', 'nDOhno1e6r', 'DRIVER'),
+	 (2, 'Wanda', 'Graham', 'wgraham4', 'b4pdWPNoMtBH', 'DRIVER'),
+	 (2, 'Sarah', 'Wood', 'swood6', 'HvDZROSZZrng', 'DRIVER'),
+	 (2, 'Christopher', 'Spencer', 'cspencer8', 'oCb182kmNmf', 'DRIVER'),
+	 (2, 'Shirley', 'Carpenter', 'scarpenteri', 'DofRXlUWDVo', 'DRIVER'),
+	 (2, 'Barbara', 'Roberts', 'brobertsj', 'x6DMIvsDbJ5r', 'DRIVER'),
+	 (2, 'Diana', 'Morgan', 'dmorganl', 'jaCbH9O4', 'DRIVER'),
+	 (2, 'Kevin', 'Rivera', 'kriveran', 'Aqtu3v9YDy', 'DRIVER'),
+	 (3, 'Ronald', 'Wallace', 'rwallace9', 'zBD64wDjPQEc', 'DISPATCHER'),
+	 (3, 'Mildred', 'Murphy', 'mmurphyc', 'J4lviq', 'DISPATCHER'),
+	 (3, 'Barbara', 'Ross', 'brossd', 'yWxcMY', 'DISPATCHER'),
+	 (3, 'Anne', 'Martin', 'amartinf', 'MtjOwHpyT', 'DISPATCHER'),
+	 (3,'Travis','Caro','tcaro','password1','DISPATCHER'),
+	 (3, 'Melissa', 'Torres', 'mtorres1', 'm8JdFqrtNR0', 'DRIVER'),
+	 (3, 'Adam', 'Matthews', 'amatthews3', 'dyKntavpwfW', 'DRIVER'),
+	 (3, 'Ralph', 'Gardner', 'rgardnerh', '6AsjdEliH1LD', 'DRIVER'),
+	 (3, 'Amy', 'Greene', 'agreene7', 'jgLl3jszgE', 'DRIVER'),
+	 (3,'Travis','Caro','tcaro_driver','password1','DRIVER');
 
 Insert into Driver
 	( ServiceID , "ID" , IsActive , IsArchived )
 	Values
-	(1,1,TRUE,FALSE),
-	(1,2,TRUE,FALSE),
-	(2,4,TRUE,FALSE),
-	(1,6,TRUE,FALSE),
-	(2,7,TRUE,FALSE),
-	(2,8,TRUE,FALSE),
-	(5,10,true,false);
+	(1,4,TRUE,FALSE),
+	(1,5,TRUE,FALSE),
+	(2,10,TRUE,FALSE),
+	(2,11,TRUE,FALSE),
+	(2,12,TRUE,FALSE),
+	(2,13,TRUE,FALSE),
+	(2,14,TRUE,FALSE),
+	(2,15,TRUE,FALSE),
+	(2,16,TRUE,FALSE),
+	(2,17,TRUE,FALSE),
+	(3,23,TRUE,FALSE),
+	(3,24,TRUE,FALSE),
+	(3,25,TRUE,FALSE),
+	(3,26,TRUE,FALSE),
+	(3,27,TRUE,FALSE);
 
 Insert into Shuttle
 	( ServiceID, "Name",IconColor, IsActive, IsArchived)
 	Values
-	(1,'BigBoyBus','#BADA55',True,False),
-	(1,'BigBoyBus2','#101010',True,False),
+	(1,'Shuttle','#BADA55',True,False),
+	(1,'Shuttle2','#6033ff',True,False),
 	(2,'Bus1','#DAD151',True,False),
-	(2,'Bus2','#FA3489',True,False);
+	(2,'Bus2','#FA3489',True,False),
+	(3,'Truck1','#FFD151',True,False),
+	(3,'Truck2','#003489',True,False);
 
 Insert into Stop
 	( ServiceID , "Name" , Address , Latitude , Longitude , IsArchived )
@@ -123,12 +147,14 @@ Insert into public.Assignment
 	Values
 	  (1,1,1,1,'2017-01-28 05:30:00','Convention Center','SCHEDULED',false),
 	  (1,2,2,2,'2017-01-28 05:30:00','Airport','SCHEDULED',false),
-	  (1,2,2,1,'2017-01-28 07:30:00','Convention Center','SCHEDULED',false);
+	  (1,2,2,1,'2017-01-28 07:30:00','Convention Center','SCHEDULED',false),
+		(2,10,3,NULL,'2017-01-28 07:30:00','2017-01-28 07:45:00','SCHEDULED',false);
 
 Insert into Assignment_Stop
-	(EstimatedTimeofArrival, EstimatedTimeofDeparture, TimeofArrival, TimeofDeparture, StopID, Address, Longitude, Latitude)
+	(AssignementID, EstimatedTimeofArrival, EstimatedTimeofDeparture, TimeofArrival, TimeofDeparture, StopID, Address, Longitude, Latitude)
 	Values
-	('2017-01-28 4:30','2017-01-28 4:35',NULL,NULL,'1','',NULL,NULL),
-	('2017-01-28 4:45','2017-01-28 4:55',NULL,NULL,NULL,'8508 Univ Blvd, 15108',NULL,NULL),
-	('2017-01-28 5:05','2017-01-28 5:20',NULL,NULL,NULL,'',40.495799, -80.255695),
-	('2017-01-28 5:30','2017-01-28 5:35',NULL,NULL,'1','',NULL,NULL);
+	(4,'2017-01-28 4:30','2017-01-28 4:35',NULL,NULL,'1','',NULL,NULL),
+	(4,'2017-01-28 4:45','2017-01-28 4:55',NULL,NULL,NULL,'8508 Univ Blvd, Moon, PA 15108',NULL,NULL),
+	(4,'2017-01-28 4:45','2017-01-28 4:55',NULL,NULL,NULL,'7500 University Blvd, Moon, PA 15108,',NULL,NULL),
+	(4,'2017-01-28 5:05','2017-01-28 5:20',NULL,NULL,NULL,'',40.495799, -80.255695),
+	(4,'2017-01-28 5:30','2017-01-28 5:35',NULL,NULL,'1','',NULL,NULL);
