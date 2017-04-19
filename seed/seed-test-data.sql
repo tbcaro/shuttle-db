51a1cc9ce49f9bd7e113e744d0f35076883b3fde
Travis Caro <travisbcaro@gmail.com>
2017-04-19 11:58:23 -0400

    Fix user / driver matching

--- seed/seed-test-data.sql
+++ seed/seed-test-data.sql
@@ -42,49 +42,40 @@ Insert into public.User
 	 (1, 'Jennifer', 'Olson', 'jolsonb', 'aaa', 'DISPATCHER'),
 	 (1, 'Katherine', 'Lopez', 'klopezg', 'aaa', 'DISPATCHER'),
 	 (1, 'Kevin', 'Palmer', 'kpalmerk', 'aaa', 'DISPATCHER'),
-	 (1, 'Amy', 'Barnes', 'abarnes0', 'aaa', 'DRIVER'),
-	 (1, 'Nancy', 'Alvarez', 'nalvareza', 'aaa', 'DRIVER'),
 	 (2, 'Matthew', 'Gutierrez', 'mgutierrez5', 'aaa', 'DISPATCHER'),
 	 (2, 'Evelyn', 'Armstrong', 'earmstronge', 'aaa', 'DISPATCHER'),
+
 	 (2, 'Pamela', 'Johnson', 'pjohnsonm', 'aaa', 'DISPATCHER'),
 	 (2, 'Wanda', 'Griffin', 'wgriffino', 'aaa', 'DISPATCHER'),
-	 (2, 'Dennis', 'Anderson', 'danderson2', 'aaa', 'DRIVER'),
-	 (2, 'Wanda', 'Graham', 'wgraham4', 'aaa', 'DRIVER'),
-	 (2, 'Sarah', 'Wood', 'swood6', 'aaa', 'DRIVER'),
-	 (2, 'Christopher', 'Spencer', 'cspencer8', 'aaa', 'DRIVER'),
-	 (2, 'Shirley', 'Carpenter', 'scarpenteri', 'aaa', 'DRIVER'),
-	 (2, 'Barbara', 'Roberts', 'brobertsj', 'aaa', 'DRIVER'),
-	 (2, 'Diana', 'Morgan', 'dmorganl', 'aaa', 'DRIVER'),
-	 (2, 'Kevin', 'Rivera', 'kriveran', 'aaa', 'DRIVER'),
-	 (3, 'Ronald', 'Wallace', 'rwallace9', 'aaa', 'DISPATCHER'),
-	 (3, 'Mildred', 'Murphy', 'mmurphyc', 'aaa', 'DISPATCHER'),
-	 (3, 'Barbara', 'Ross', 'brossd', 'aaa', 'DISPATCHER'),
-	 (3, 'Anne', 'Martin', 'amartinf', 'aaa', 'DISPATCHER'),
+		(3,'Test','Dispatcher1','test_dispatcher1','aaa','DISPATCHER'),
+		(3,'Test','Dispatcher2','test_dispatcher2','aaa','DISPATCHER'),
+		(3,'Test','Dispatcher3','test_dispatcher3','aaa','DISPATCHER'),
+
+		(1, 'Amy', 'Barnes', 'abarnes0', 'aaa', 'DRIVER'),
+		(1, 'Nancy', 'Alvarez', 'nalvareza', 'aaa', 'DRIVER'),
+		(2, 'Dennis', 'Anderson', 'danderson2', 'aaa', 'DRIVER'),
+		(2, 'Wanda', 'Graham', 'wgraham4', 'aaa', 'DRIVER'),
+		(2, 'Diana', 'Morgan', 'dmorganl', 'aaa', 'DRIVER'),
+
+		(2, 'Kevin', 'Rivera', 'kriveran', 'aaa', 'DRIVER'),
 	 (3, 'Melissa', 'Torres', 'mtorres1', 'aaa', 'DRIVER'),
-	 (3, 'Adam', 'Matthews', 'amatthews3', 'aaa', 'DRIVER'),
-	 (3, 'Ralph', 'Gardner', 'rgardnerh', 'aaa', 'DRIVER'),
-	 (3, 'Amy', 'Greene', 'agreene7', 'aaa', 'DRIVER'),
-		(3,'Test','Dispatcher','test_dispatcher','aaa','DISPATCHER'),
-	 (3,'Test','Driver','test_driver','aaa','DRIVER');
+	 (3,'Test','Driver1','test_driver1','aaa','DRIVER'),
+		(3,'Test','Driver2','test_driver2','aaa','DRIVER'),
+		(3,'Test','Driver3','test_driver3','aaa','DRIVER');
 
 Insert into Driver
 	( ServiceID , "ID" , IsActive , IsArchived )
 	Values
-	(1,4,TRUE,FALSE),
-	(1,5,TRUE,FALSE),
-	(2,10,TRUE,FALSE),
-	(2,11,TRUE,FALSE),
-	(2,12,TRUE,FALSE),
+	(1,11,TRUE,FALSE),
+	(1,12,TRUE,FALSE),
 	(2,13,TRUE,FALSE),
 	(2,14,TRUE,FALSE),
 	(2,15,TRUE,FALSE),
 	(2,16,TRUE,FALSE),
-	(2,17,TRUE,FALSE),
-	(3,23,TRUE,FALSE),
-	(3,24,TRUE,FALSE),
-	(3,25,TRUE,FALSE),
-	(3,26,TRUE,FALSE),
-	(3,27,TRUE,FALSE);
+	(3,17,TRUE,FALSE),
+	(3,18,TRUE,FALSE),
+	(3,19,TRUE,FALSE),
+	(3,20,TRUE,FALSE);
 
 Insert into Shuttle
 	( ServiceID, "Name",IconColor, IsActive, IsArchived)
